c6e50acc432a023c5597bb3b584749cbf6ff5a87
tokiniaina <hei.tokiniaina@gmail.com>
2025-09-04 12:10:42 +0300

    feat:add some modif

--- backend/settings.py
+++ backend/settings.py
@@ -139,6 +139,10 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 # settings.py
 X_FRAME_OPTIONS = 'SAMEORIGIN'  # autorise les iframes sur le même domaine
-ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS", "127.0.0.1")]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "dms-backend.onrender.com",   # ton backend Render
+]
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
